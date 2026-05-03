908af9e4496349d108f38302108160ccd6fde011
Moe Mousavi <itsmohammadmoosavi@gmail.com>
2026-05-03 10:15:49 -0400

    feat: initial project setup and config

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <nav></nav>
+      <main>
+        <section id="center">
+          <div className="hero"></div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
